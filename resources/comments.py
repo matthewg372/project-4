695841803abd8521c41eda2325fcdf800d6000d0
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-05-08 10:01:48 -0500

    update and edit completed

--- resources/comments.py
+++ resources/comments.py
@@ -42,3 +42,29 @@ def delete_comments(id):
 			message="you must be logged in to delete this",
 			status=403
 		), 403
+
+@comments.route('/<id>', methods=['PUT'])
+def update_product(id):
+	payload = request.get_json()
+	comment_to_update = models.Comment.get_by_id(id)
+	if current_user.id == comment_to_update.user.id:
+		if 'bio' in payload:
+			comment_to_update.bio=payload['bio']
+		comment_to_update.save()
+		updated_comment_dict = model_to_dict(comment_to_update)
+		updated_comment_dict['user'].pop('password')
+		return jsonify(
+			data=updated_comment_dict,
+			message=f"succesfully updated {id}",
+			status=200
+		),200
+	else:
+		return jsonify(
+			data={},
+			message=f"you must be logged in to updated",
+			status=403
+		), 403
+
+
+
+
