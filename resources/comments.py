c905c1189168a832c1f95e460f455584f93922a2
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-05-08 09:59:13 -0500

    deleting specific comment

--- resources/comments.py
+++ resources/comments.py
@@ -18,10 +18,27 @@ def create_comment(id):
 	)
 	comment_dict = model_to_dict(new_comments)
 	comment_dict['user'].pop('password')
-	comment_dict['user'].pop('password')
 
 	return jsonify(
 		data=comment_dict,
 		message="successfull commented",
 		status=200
 	),200
+
+@comments.route('/<id>', methods=['DELETE'])
+def delete_comments(id):
+	comment_to_delete = models.Comment.get_by_id(id)
+	if current_user.id == comment_to_delete.user.id:
+		delete_query = models.Comment.delete().where(models.Comment.id == id)
+		delete_query.execute()
+		return jsonify(
+			data={},
+			message=f"succesfully deleted {id}",
+			status=200
+		), 200
+	else:
+		return jsonify(
+			data={},
+			message="you must be logged in to delete this",
+			status=403
+		), 403
