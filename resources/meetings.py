fb027f568d1437075821a80db42776e1289b4cc3
matthewg372 <59273107+matthewg372@users.noreply.github.com>
2020-05-11 16:41:38 -0500

    deleting meetings is working

--- resources/meetings.py
+++ resources/meetings.py
@@ -5,7 +5,17 @@ from flask_login import current_user, login_required
 
 meetings = Blueprint('meetings', 'meetings')
 
-
+@meetings.route('/all', methods=['GET'])
+def all_profiles():
+	meetings = models.Meeting.select()
+	meetings_dicts = [model_to_dict(meeting) for meeting in meetings]
+	for meeting_dict in meetings_dicts:
+		meeting_dict['user'].pop('password')
+	return jsonify(
+		data=meetings_dicts,
+		message="successfully showed all meetings",
+		status=200
+	),200
 
 @meetings.route('/', methods=['POST'])
 @login_required
@@ -25,3 +35,28 @@ def create_meeting():
 		message="successfully posted",
 		status=200
 	),200
+
+@meetings.route('/<id>', methods=['DELETE'])
+@login_required
+def delete_meeting(id):
+	meeting_to_delete = models.Meeting.get_by_id(id)
+	if current_user.id == meeting_to_delete.user.id:
+		delete_query = models.Meeting.delete().where(models.Meeting.id == id)
+		delete_query.execute()
+		print('deleted')
+		return jsonify(
+			data={},
+			message=f"successfully deleted {id}",
+			status=200
+		), 200
+	else:
+		return jsonfiy(
+			data={},
+			message="you must be logged in",
+			status=403
+			),403
+
+
+
+
+
